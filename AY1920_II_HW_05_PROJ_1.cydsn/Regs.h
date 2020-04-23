19f1cb351b4f22e963f0a99d59845b52eae61039
Alessio <alessio1.ratti@mail.polimi.it>
2020-04-23 13:09:28 +0200

    Project 1: Register addresses moved from main.c to Regs.h

---
+++ AY1920_II_HW_05_PROJ_1.cydsn/Regs.h
@@ -0,0 +1,57 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+/*******************************************/
+/*       Sorted registers of interest      */
+/*******************************************/
+
+/**
+*   \brief Address of the ADC output LSB register
+*/
+#define LIS3DH_OUT_ADC_3L 0x0C
+
+/**
+*   \brief Address of the ADC output MSB register
+*/
+#define LIS3DH_OUT_ADC_3H 0x0D
+
+/**
+*   \brief Address of the WHO AM I register
+*/
+#define LIS3DH_WHO_AM_I_REG_ADDR 0x0F
+
+/**
+*   \brief 7-bit I2C address of the slave device.
+*/
+#define LIS3DH_DEVICE_ADDRESS 0x18
+
+/**
+*   \brief  Address of the Temperature Sensor Configuration register
+*/
+#define LIS3DH_TEMP_CFG_REG 0x1F
+
+/**
+*   \brief Address of the Control register 1
+*/
+#define LIS3DH_CTRL_REG1 0x20
+
+/**
+*   \brief Address of the Control register 4
+*/
+#define LIS3DH_CTRL_REG4 0x23
+
+/**
+*   \brief Address of the Status register
+*/
+#define LIS3DH_STATUS_REG 0x27
+
+/* [] END OF FILE */
--- AY1920_II_HW_05_PROJ_1.cydsn/main.c
+++ AY1920_II_HW_05_PROJ_1.cydsn/main.c
@@ -13,57 +13,16 @@
 #include "I2C_Interface.h"
 #include "project.h"
 #include "stdio.h"
+#include "Regs.h"
 
 #define LENGTH_T 2  // number of bytes to store temp
 
-/**
-*   \brief 7-bit I2C address of the slave device.
-*/
-#define LIS3DH_DEVICE_ADDRESS 0x18
-
-/**
-*   \brief Address of the WHO AM I register
-*/
-#define LIS3DH_WHO_AM_I_REG_ADDR 0x0F
-
-/**
-*   \brief Address of the Status register
-*/
-#define LIS3DH_STATUS_REG 0x27
-
-/**
-*   \brief Address of the Control register 1
-*/
-#define LIS3DH_CTRL_REG1 0x20
-
-/**
-*   \brief Hex value to set normal mode to the accelerator
-*/
+// set normal mode to the accelerator
 #define LIS3DH_NORMAL_MODE_CTRL_REG1 0x47
-
-/**
-*   \brief  Address of the Temperature Sensor Configuration register
-*/
-#define LIS3DH_TEMP_CFG_REG 0x1F
 
 #define LIS3DH_TEMP_CFG_REG_ACTIVE 0xC0
 
-/**
-*   \brief Address of the Control register 4
-*/
-#define LIS3DH_CTRL_REG4 0x23
-
 #define LIS3DH_CTRL_REG4_BDU_ACTIVE 0x80
-
-/**
-*   \brief Address of the ADC output LSB register
-*/
-#define LIS3DH_OUT_ADC_3L 0x0C
-
-/**
-*   \brief Address of the ADC output MSB register
-*/
-#define LIS3DH_OUT_ADC_3H 0x0D
 
 int main(void)
 {
